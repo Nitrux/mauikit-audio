3b4b0e2cdcd2d311b88e68d5a402c1da47af5ed8
Uri Herrera <hello@uriherrera.com>
2025-10-16 16:28:49 -0600

    add neon repo

--- scripts/install-build-deps.sh
+++ scripts/install-build-deps.sh
@@ -37,10 +37,38 @@ else
 fi
 
 
+# -- Add third-aprty repository keys.
+
+add_repo_key_and_source() {
+    local key_id="$1"
+    local keyring_file="$2"
+    local source_file="$3"
+    local source_content="$4"
+
+    if ! gpg --list-keys "$key_id" &>/dev/null; then
+        gpg --batch --keyserver hkps://keyserver.ubuntu.com --recv-keys "$key_id"
+    fi
+
+    if ! [ -f "$keyring_file" ] || ! gpg --quiet --with-colons --import-options show-only --import "$keyring_file" | grep -q "$key_id"; then
+        gpg --batch --yes --export "$key_id" | gpg --dearmor -o "$keyring_file"
+    fi
+
+    if ! grep -Fxq "$source_content" "$source_file" 2>/dev/null; then
+        echo "$source_content" > "$source_file"
+    fi
+}
+
+add_repo_key_and_source \
+    "E6D4736255751E5D" \
+    "/etc/apt/keyrings/kde_neon-archive-keyring.gpg" \
+    "/etc/apt/sources.list.d/neon-repo.list" \
+    "deb [signed-by=/etc/apt/keyrings/kde_neon-archive-keyring.gpg] https://origin.archive.neon.kde.org/stable/ jammy main"
+
+
 # -- Install build packages.
 
 $APT_COMMAND update -q
-$APT_COMMAND install -y - --no-install-recommends \
+$APT_COMMAND install -qy - --no-install-recommends \
     appstream \
     automake \
     autotools-dev \
